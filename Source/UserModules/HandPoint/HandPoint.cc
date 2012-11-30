c192c3b4affc9512bb02918b49d9b69fa3e26d3e
Lars Nyström <lars.nystrom@live.com>
2012-11-30 21:54:35 +0100

    Added module HandPoint.

---
+++ Source/UserModules/HandPoint/HandPoint.cc
@@ -0,0 +1,105 @@
+//
+//	HandPoint.cc		This file is a part of the IKAROS project
+//
+//    Copyright (C) 2012 <Author Name>
+//
+//    This program is free software; you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation; either version 2 of the License, or
+//    (at your option) any later version.
+//
+//    This program is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with this program; if not, write to the Free Software
+//    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+//
+//    See http://www.ikaros-project.org/ for more information.
+//
+
+
+#include "HandPoint.h"
+
+using namespace ikaros;
+
+void
+HandPoint::Init()
+{
+    handPoint = GetOutputArray("HAND_POINT");   
+    handPointView = GetOutputArray("HAND_POINT_VIEW");    
+    
+    OpenNi& on = OpenNi::getInstance();
+    on.StartGeneratingAll();
+    
+    m_HandTracker->Run();
+}
+
+float
+HandPoint::xCon(float xi, float zi){
+  float xo = (320*xi)/(zi*tan(0.4974)) + 320;
+  return xo;
+}
+
+float
+HandPoint::yCon(float yi, float zi){
+  float yo = (240*yi)/(zi*tan(0.3752)) + 240;
+  return yo;
+}
+
+
+void
+HandPoint::Tick()
+{
+	niRetVal = niContext.WaitAndUpdateAll();
+	if (niRetVal != XN_STATUS_OK)
+	{
+		printf("Read failed: %s\n", xnGetStatusString(niRetVal));
+		return;
+	}
+	
+	typedef TrailHistory			History;
+	typedef History::ConstIterator	HistoryIterator;
+	typedef Trail::ConstIterator	TrailIterator;
+
+	static const float colours[][3] =
+	{
+		{ 0.5f, 0.5f, 0.5f},
+		{ 0.0f, 1.0f, 0.0f},
+		{ 0.0f, 0.5f, 1.0f},
+		{ 1.0f, 1.0f, 0.0f},
+		{ 1.0f, 0.5f, 0.0f},
+		{ 1.0f, 0.0f, 1.0f}
+	};
+	const TrailHistory&	history = m_HandTracker->GetHistory();
+
+	// History points coordinates buffer
+	XnFloat	coordinates[3 * MAX_HAND_TRAIL_LENGTH];
+
+	const HistoryIterator	hend = history.End();
+	for(HistoryIterator		hit = history.Begin(); hit != hend; ++hit)
+	{
+		// Dump the history to local buffer
+		const Trail&	trail = hit->Value();
+		
+		TrailIterator last = trail.Begin();
+		XnPoint3D point = *last;
+		
+		handPoint[0] = xCon(point.X, point.Z);
+		handPoint[1] = yCon(point.Y, point.Z);
+		handPoint[2] = point.Z;
+		
+		handPointView[0] = handPoint[0] / 640.f;
+		handPointView[1] = handPoint[1] / 480.f;
+		
+		printf("HandPoint: (%f, %f, %f)\t\t(%f, %f, %f)\n", point.X, point.Y, point.Z, handPoint[0], handPoint[1], handPoint[2]);
+	}
+}
+
+
+
+static InitClass init("HandPoint", &HandPoint::Create, "Source/UserModules/HandPoint/");
+
+
---
+++ Source/UserModules/HandPoint/HandPoint.h
@@ -0,0 +1,68 @@
+//
+//	HandPoint.h		This file is a part of the IKAROS project
+// 						
+//    Copyright (C) 2012 <Author Name>
+//
+//    This program is free software; you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation; either version 2 of the License, or
+//    (at your option) any later version.
+//
+//    This program is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with this program; if not, write to the Free Software
+//    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+//
+//    See http://www.ikaros-project.org/ for more information.
+//
+
+
+#ifndef HandPoint_
+#define HandPoint_
+
+#include "IKAROS.h"
+#include "./../OpenNi/OpenNi.h"
+#include "NiHandTracker.h"
+#include <XnCppWrapper.h>
+
+class HandPoint: public Module
+{
+public:
+    float *     handPoint;
+    float *     handPointView;
+    static Module * Create(Parameter * p) { return new HandPoint(p); }
+
+    HandPoint(Parameter * p) : Module(p) 
+    {
+        OpenNi& on = OpenNi::getInstance();
+        niContext = on.getContext();
+        
+        m_HandTracker = new HandTracker(niContext);
+        
+        niRetVal = m_HandTracker->Init();
+        
+    }
+    
+    virtual ~HandPoint() 
+    {
+        delete m_HandTracker;
+    }
+
+    void 		Init();
+    void 		Tick();
+    
+private: 
+    float xCon(float xi, float zi);
+    float yCon(float yi, float zi);
+    
+    xn::Context     niContext;
+    HandTracker*    m_HandTracker;
+    XnStatus        niRetVal;
+};
+
+#endif
+
