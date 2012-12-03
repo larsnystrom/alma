1031f9bdc0543a3dab391bff72465b40bc095282
Lars Nyström <lars.nystrom@live.com>
2012-12-03 14:39:25 +0100

    Major update to handle both Hand gestures and Head pose.

--- Source/UserModules/PrioMod/PrioMod.cc
+++ Source/UserModules/PrioMod/PrioMod.cc
@@ -7,31 +7,183 @@ float
 PrioMod::dist(float x1, float y1, float z1, float x2, float y2, float z2){
   float d;
 
-  d = pow((x1-x2),2) + pow((y1-y2),2) + pow((z1-z2),2);
+  d = pow((x1-x2), 2) + pow((y1-y2), 2) + pow((z1-z2), 2);
   d = sqrt(d);
 
   return d;
 }
 
+/**
+ *Ex:
+ *1=Head center
+ *2=Face center
+ *3=AR-tagg location
+ **/
+float
+PrioMod::linePlaneIntersect(
+    float x1, float y1, float z1, 
+    float x2, float y2, float z2, 
+    float x3, float y3, float z3)
+{
+    float num = x1 * ( (z3/x3) + (z3*y3)/(2*x3*x3) + z3/(2*x3*x3) ) - y1*( z3/(2*x3) ) - z1 - z3/(2*x3);
+    
+    float denom = z2 - z1 - (x2-x1)*( (z3/x3) + (z3*y3)/(2*x3*x3) + z3/(2*x3*x3) ) + (y2-y1)*( z3/(2*x3) );
+    
+    float dl = dist( 
+        (x1 + (num/denom)*(x2-x1)), 
+        (y1 + (num/denom)*(y2-y1)), 
+        (z1 + (num/denom)*(z2-z1)), 
+        x3, 
+        y3, 
+        z3
+    );
+    
+    return dl;
+}
+
+int
+PrioMod::attendedMarker(float* center, float* front, float threshold)
+{
+    float curDist = 0.0f;
+    float minDist = threshold;
+    int markerId = -1;
+    
+    for (int i = 0; i < maxMarkers; ++i) {
+        
+        if (markers[i][2] > 0.1f) {
+            curDist = linePlaneIntersect(
+                center[0], center[1], center[2], 
+                front[0], front[1], front[2],
+                markers[i][0], markers[i][1], markers[i][23] / 1000.f
+            );
+            
+            if (curDist < minDist) {
+                markerId = i;
+                minDist = curDist;
+            }
+        }
+    }
+    
+    return markerId;
+}
+
+void
+PrioMod::lookAtDefault()
+{
+    attended[0] = 0.5f;
+    attended[1] = 0.5f;
+    attended[2] = 2.0f;
+}
+
+void
+PrioMod::lookAtHead()
+{
+    attended[0] = head_center[0];
+    attended[1] = head_center[1];
+    attended[2] = head_center[2];
+}
+
+void
+PrioMod::lookAtHand()
+{
+    attended[0] = hand_center[0];
+    attended[1] = hand_center[1];
+    attended[2] = hand_center[2];
+}
+
+void
+PrioMod::lookAtMarker(int markerId)
+{
+    attended[0] = markers[markerId][0];
+    attended[1] = markers[markerId][1];
+    attended[2] = markers[markerId][23] / 1000.f;
+}
+
 void
 PrioMod::Init(){
-  markers = GetInputMatrix("ATTENDABLES");
-  maxMarkers = 10;
-  clocam = GetInputArray("CLOSEST");
-  attended = GetOutputArray("ATTENDED");
+    maxMarkers = GetIntValue("max_markers");
+    
+    headPoseAttendMaxDist = GetFloatValue("HEAD_ATTEND_MAX_DIST");   
+    printf("PrioMod: HEAD_ATTEND_MAX_DIST=%f\n", headPoseAttendMaxDist);
+    
+    markers = GetInputMatrix("ATTENDABLES");
+    head_center = GetInputArray("HEAD_CENTER");
+    head_front = GetInputArray("HEAD_FRONT");
+    
+    
+    handPoseAttendMaxDist = GetFloatValue("HAND_ATTEND_MAX_DIST");
+    printf("PrioMod: HAND_ATTEND_MAX_DIST=%f\n", handPoseAttendMaxDist);
+    
+    hand_center = GetInputArray("HAND_CENTER");
+    hand_front = GetInputArray("HAND_FRONT");
+    hand_wave = GetInputArray("HAND_WAVE");
+    
+    attended = GetOutputArray("ATTENDED");
+    attendedView = GetOutputArray("ATTENDED_VIEW");
 }
 
 void
 PrioMod::Tick(){
-  float oldClo = 1;
-  for(int i=0; i < maxMarkers; ++i){
-    if(dist(markers[i][0], markers[i][1], 0, clocam[0], clocam[1], 0)<oldClo){
-      attended[0]=markers[i][0];
-      attended[1]=markers[i][1];
-      attended[2]=markers[i][23]/1000;
-      oldClo=dist(markers[i][0], markers[i][1], 0, clocam[0], clocam[1], 0);
+    int headMarker = -1;
+    int handMarker = -1;
+    bool headExists = false;
+    bool handExists = false;
+    
+    if (head_center[0] > 0.00000000001f) {
+        headMarker = attendedMarker(head_center, head_front, headPoseAttendMaxDist);
+        headExists = true;
+    } 
+    
+    if (hand_center[0] > 0.00000000001f) {
+        handMarker = attendedMarker(hand_center, hand_front, handPoseAttendMaxDist);
+        handExists = true;
     }
-  }
+    
+    if (false == handExists && false == headExists) {
+        lookAtDefault();
+    }
+    
+    if (true == headExists && false == handExists) {
+        if (-1 != headMarker) {
+            lookAtMarker(headMarker);
+        } else {
+            lookAtHead();
+        }
+    }
+    
+    if (false == headExists && true == handExists) {
+        if (-1 != handMarker) {
+            lookAtMarker(handMarker);
+        } else {
+            lookAtDefault();
+        }
+    }
+    
+    if (true == headExists && true == handExists) {
+        if (-1 == headMarker && -1 == handMarker) {
+            lookAtHead();
+        }
+        
+        if (-1 == headMarker && -1 != handMarker) {
+            lookAtMarker(handMarker);
+        }
+        
+        if (-1 != headMarker && -1 == handMarker) {
+            lookAtMarker(headMarker);
+        }
+        
+        if (-1 != headMarker && -1 != handMarker) {
+            if (headMarker == handMarker) {
+                lookAtMarker(headMarker);
+            } else {
+                lookAtDefault();
+            }
+        }
+    }
+    
+    attendedView[0] = attended[0];
+    attendedView[1] = attended[1];
+  
 }
 
 static InitClass init("PrioMod", &PrioMod::Create, "Source/UserModules/PrioMod/");
--- Source/UserModules/PrioMod/PrioMod.h
+++ Source/UserModules/PrioMod/PrioMod.h
@@ -8,6 +8,8 @@ class PrioMod: public Module
 public:
     static Module * Create(Parameter * p) { return new PrioMod(p); }
     float dist(float x1, float y1, float z1, float x2, float y2, float z2);
+    float linePlaneIntersect(float x1, float y1, float z1, float x2, float y2, float z2, float x3, float y3, float z3);
+    int attendedMarker(float* center, float* front, float threshold);
 
     PrioMod(Parameter * p) : Module(p) {}
     virtual ~PrioMod() {}
@@ -21,6 +23,20 @@ public:
 
     float * clocam;
     float * attended;
+    float * head_center;
+    float * head_front;
+    float * hand_center;
+    float * hand_front;
+    float * hand_wave;
+    float * attendedView;
+private:
+    float headPoseAttendMaxDist;
+    float handPoseAttendMaxDist;
+    
+    void lookAtDefault();
+    void lookAtHead();
+    void lookAtHand();
+    void lookAtMarker(int markerId);
 };
 
 #endif
